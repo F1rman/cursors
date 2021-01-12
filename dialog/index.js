d8811a6b0eecee0f5c4caaaffad8e0afc28fad88
F1rman <ihorkharchyshyn@gmail.com>
2021-01-12 21:14:00 +0200

    new cursors rate me

--- dialog/index.js
+++ dialog/index.js
@@ -10,7 +10,7 @@ current_cursor_id = -1;
 cursor_array_elements=[];
 
 //Change in 2 places
-cursor_array_ids = ["cursor0001", "cursor0002", "cursor0003", "cursor0004","cursor0005","cursor0006","cursor0007", "cursor0008", "cursor0009", "cursor0010","cursor0011","cursor0012","cursor0013","cursor0014","cursor0015","cursor0016","cursor0017","cursor0018","cursor0019","cursor0020","cursor0021","cursorthings_0001","cursorthings_0002","cursorthings_0003","cursorthings_0004","cursorthings_0005","cursorthings_0006","cursorthings_0007","cursorthings_0008","cursorthings_0009","cursorthings_0010","cursorthings_0011","cursorthings_0012","cursorthings_0013","cursorthings_0014","cursorthings_0015","cursorthings_0016","cursorthings_0017","cursorthings_0018","cursorthings_0019","cursorthings_0020"];
+cursor_array_ids = ["cursor0001", "cursor0002", "cursor0003", "cursor0004","cursor0005","cursor0006","cursor0007", "cursor0008", "cursor0009", "cursor0010","cursor0011","cursor0012","cursor0013","cursor0014","cursor0015","cursor0016","cursor0017","cursor0018","cursor0019","cursor0020","cursor0021","cursorthings_0001","cursorthings_0002","cursorthings_0003","cursorthings_0004","cursorthings_0005","cursorthings_0006","cursorthings_0007","cursorthings_0008","cursorthings_0009","cursorthings_0010","cursorthings_0011","cursorthings_0012","cursorthings_0013","cursorthings_0014","cursorthings_0015","cursorthings_0016","cursorthings_0017","cursorthings_0018","cursorthings_0019","cursorthings_0020","cursorthings_0021"];
 remove_class_str='';
 remove_class_str_with_spaces='';
 
